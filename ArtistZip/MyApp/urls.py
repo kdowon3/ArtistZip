b9226bc51dbfebe88298396cc5ed57f319e64a8a
s4n9hyun <s4n9hyun@gmail.com>
2024-07-04 15:29:35 +0900

    login page

--- ArtistZip/MyApp/urls.py
+++ ArtistZip/MyApp/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name='signup'),
     path('login/', views.login, name='login'),
+    path('artist_signup/', views.artist_signup, name='artist_signup'),
+    path('normal_signup/', views.normal_signup, name='normal_signup'),
+    path('profile/', views.profile, name='profile'),
 ]
