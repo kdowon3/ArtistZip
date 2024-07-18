a84a6cdc6de71a76d9c6ab7b6c249c3d86c7b8ae
kdowon3 <dowon8849@gmail.com>
2024-07-18 16:28:51 +0900

    envaddedforsecurity

--- ArtistZip/ArtistZip/settings.py
+++ ArtistZip/ArtistZip/settings.py
@@ -171,10 +171,12 @@ SOCIALACCOUNT_ADAPTER = 'Auth.adapter.CustomSocialAccountAdapter'
 #chatsetting#
 ASGI_APPLICATION = 'ArtistZip.asgi.application'
 
+
+
 CACHES = {
     'default': {
         'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'LOCATION': f'redis://{env("REDIS_HOST")}:{env("REDIS_PORT")}/1',
         'OPTIONS': {
             'CLIENT_CLASS': 'django_redis.client.DefaultClient',
         }
@@ -185,7 +187,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [(env("REDIS_HOST"), env("REDIS_PORT"))],
         },
     },
 }
